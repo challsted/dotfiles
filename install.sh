120857dcaf6d2fcc6cb54e39127de4581592b8a0
hackinginformation <chase.hallsted2012@gmail.com>
2014-09-02 14:51:11 -0500

    unexpected newline error with curl and wget, fix?

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 ##Filename: install.sh
 ##Author: HackingInformation
 ##Contact: https://github.com/hackinginformation
-##Version 1.1
+##Version 1.1.1 - Trying to fix issue with wget/curl
 #
 # An attempt at makeing an install for the files I upload
 # YOU WILL NEED TO CHMOD THIS FILE (chmod +x install.sh)
@@ -27,9 +27,7 @@ NORMAL=$(tput sgr 0)
 BLINK=$(tput blink)
 REVERSE=$(tput smso)
 UNDERLINE=$(tput smul)
-
 cd ~
-
 printf "${WHITE}You need to install git, zsh, wget, curl and vim, if you havnt already${NORMAL}\n"
 printf "${WHITE}This will only work on ubuntu based OS's currently${NORMAL}\n"
 printf "${WHITE}Select \"n\" if you are not useing an ubuntu based OS${NORMAL}\n"
@@ -43,7 +41,6 @@ while true; do
         * ) printf "${RED}Please answer \"y\"es or \"n\"o${NORMAL}\n";; 
     esac
 done
-
 GIT_DIRECTORY="$HOME/git"
 read -p "${CYAN}Do you want a regular folder for git or a hidden folder? (R/h)${NORMAL}" rh
     if [[ "$rh" == "H" ]] || [[ "$rh" == "h" ]]; then
@@ -52,15 +49,12 @@ read -p "${CYAN}Do you want a regular folder for git or a hidden folder? (R/h)${
         mkdir -p $GIT_DIRECTORY;
         printf "${CYAN} Ok, I have created $GIT_DIRECTORY for you${NORMAL}\n";
         cd $GIT_DIRECTORY;
-
 printf "${WHITE}Im about to install \"Oh-My-Zsh\", if you want to look into this${NORMAL}\n"
 printf "${WHITE}go to their github https://github.com/robbyrussell/oh-my-zsh${NORMAL}\n"
 printf "${WHITE}Installing Oh-My-Zsh now!${NORMAL}\n"
 wget --no-check-certificate http://install.ohmyz.sh >/dev/null 2>&1 | sh 
-
 printf "${WHITE}Cloneing my dotfiles repo now${NORMAL}\n"
 git clone --quiet https://github.com/hackinginformation/mydotfiles.git
-
 printf "${WHITE}Doing some background work${NORMAL}\n"
 cd $HOME
 cp $GIT_DIRECTORY/mydotfiles/zsh/.zshrc .
@@ -72,7 +66,6 @@ curl --silent -fLo ~/.vim/autoload/plug.vim \
 printf "${WHITE}You are about to be presented with a vim script updater${NORMAL}\n"
 printf "${WHITE}Please wait for it to complete, then run \":q\" TWO TIMES to exit and continue${NORMAL}\n"
 vim -c :PlugInstall #Calls vim-plug to install plugins
-
 printf "${WHITE}You must change your shell to ZSH (Z-Shell) now${NORMAL}\n"
 while true; do 
     read -p "${CYAN}Would you like me to change this for you? (Y/n)${NORMAL}" yn
@@ -85,7 +78,6 @@ while true; do
         * ) printf "${RED}Please answer \"y\"es or \"n\"o${NORMAL}\n";;
     esac
 done
-
 printf "${WHITE}Please restart your shell, once you reopen, please enter option \"0\"${NORMAL}\n"
 printf "${WHITE}If you do not, it will overwrite your your .zshrc file with a default one${NORMAL}\n"
 exit
