2e5ddc052236c06ab42cd873f47ac1ffd5abe7e3
hackinginformation <chase.hallsted2012@gmail.com>
2014-09-02 13:48:15 -0500

    added option to just press enter and colors fixed version 1.1-alpha

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 ##Filename: install.sh
 ##Author: HackingInformation
 ##Contact: https://github.com/hackinginformation
-##Version 0.11 (This might be incorrect, as i sometimes forget to update)
+##Version 1.1-alpha 
 #
 # An attempt at makeing an install for the files I upload
 # YOU WILL NEED TO CHMOD THIS FILE (chmod +x install.sh)
@@ -23,44 +23,45 @@ MAGENTA=$(tput setaf 5)
 CYAN=$(tput setaf 6)
 WHITE=$(tput setaf 7)
 BRIGHT=$(tput bold)
-NORMAL=$(tput sgr0)
+NORMAL=$(tput srg0)
 BLINK=$(tput blink)
 REVERSE=$(tput smso)
 UNDERLINE=$(tput smul)
 
 cd ~
 
-printf "$(tput setaf 1)${BLUE}You need to install git, zsh, wget, curl and vim, if you havnt already$(tput sgr0)\n"
-printf "$(tput setaf 1)This will only work on ubuntu based OS's currently$(tput sgr0)\n"
-printf "$(tput setaf 1)Select \"n\" if you are not useing an ubuntu based OS$(tput sgr0)\n"
+printf "${RED}You need to install git, zsh, wget, curl and vim, if you havnt already${NORMAL}\n"
+printf "${RED}This will only work on ubuntu based OS's currently${NORMAL}\n"
+printf "${RED}Select \"n\" if you are not useing an ubuntu based OS${NORMAL}\n"
 while true; do 
-    read -p "$(tput setaf 1)Would you like me to install them for you? (Y/n)$(tput sgr0)" yn
+    read -p "${CYAN}Would you like me to install them for you? (Y/n)${NORMAL}" yn
     case $yn in
-        [Yy]* ) printf "$(tput setaf 1)Ok I am installing git, zsh and vim for you$(tput sgr0)\n";
+        [Yy]* ) printf "${CYAN}Ok I am installing git, zsh and vim for you${NORMAL}\n";
             sudo apt-get -y install git zsh wget curl vim > /dev/null; break;;
-        [Nn]* ) printf "Ok, skipping the installs$(tput sgr0)\n"; break;;
-        * ) printf "$(tput setaf 1)Please answer yes or no$(tput sgr0)\n";; 
+        [Nn]* ) printf "${BLUE}Ok, skipping the installs${NORMAL}\n"; break;;
+        "" ) sudo apt-get -y install git zsh wget curl vim > /dev/null; break;;
+        * ) printf "${BLUE}Please answer \"y\"es or \"n\"o${NORMAL}\n";; 
     esac
 done
 
 GIT_DIRECTORY="$HOME/git"
-read -p "$(tput setaf 1)Do you want a regular folder for git or a hidden folder? (R/h)$(tput sgr0)" rh
-if [[ "$rh" == "H" ]] || [[ "$rh" == "h" ]]; then
-    GIT_DIRECTORY="$HOME/.git"
-fi    
-    mkdir -p $GIT_DIRECTORY;
-    cd $GIT_DIRECTORY;
+read -p "${CYAN}Do you want a regular folder for git or a hidden folder? (R/h)${NORMAL}" rh
+    if [[ "$rh" == "H" ]] || [[ "$rh" == "h" ]]; then
+        GIT_DIRECTORY="$HOME/.git"
+    fi    
+        mkdir -p $GIT_DIRECTORY;
+        printf "${CYAN} Ok, I have created $GIT_DIRECTORY for you${NORMAL}";
+        cd $GIT_DIRECTORY;
 
-
-printf "$(tput setaf 1)Im about to install \"Oh-My-Zsh\", if you want to look into this$(tput sgr0)\n"
-printf "$(tput setaf 1)go to their github https://github.com/robbyrussell/oh-my-zsh$(tput sgr0)\n"
-printf "$(tput setaf 1)Installing Oh-My-Zsh now!$(tput sgr0)\n"
+printf "${RED}Im about to install \"Oh-My-Zsh\", if you want to look into this${NORMAL}\n"
+printf "${RED}go to their github https://github.com/robbyrussell/oh-my-zsh${NORMAL}\n"
+printf "${RED}Installing Oh-My-Zsh now!${NORMAL}\n"
 wget --no-check-certificate http://install.ohmyz.sh >/dev/null 2>&1 | sh 
 
-printf "$(tput setaf 1)Cloneing my dotfiles repo now$(tput sgr0)\n"
+printf "${RED}Cloneing my dotfiles repo now${NORMAL}\n"
 git clone --quiet https://github.com/hackinginformation/mydotfiles.git
 
-printf "$(tput setaf 1)Doing some background work$(tput sgr0)\n"
+printf "${RED}Doing some background work${NORMAL}\n"
 cd $HOME
 cp $GIT_DIRECTORY/mydotfiles/zsh/.zshrc .
 sed -i "s,RePlAcE,$GIT_DIRECTORY,g" "$HOME/.zshrc"
@@ -68,21 +69,23 @@ ln -sf $GIT_DIRECTORY/mydotfiles/vim/.vimrc ~/.vimrc #Symlinks for the .vimrc fi
 mkdir -p ~/.vim/autoload # Preps for vim-plug to be installed
 curl --silent -fLo ~/.vim/autoload/plug.vim \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-printf "$(tput setaf 1)You are about to be presented with a vim script updater$(tput sgr0)\n"
-printf "$(tput setaf 1)Please wait for it to complete, then run :q two to exit and continue$(tput sgr0)\n"
-vim -c :PlugInstall #Calls vim-plug to install plugins ###BROKEN
+printf "${YELLOW}You are about to be presented with a vim script updater${NORMAL}\n"
+printf "${YELLOW}Please wait for it to complete, then run ${NORMAL}${BLUE}\":q\" TWO TIMES${NORMAL}${YELLOW} to exit and continue${NORMAL}\n"
+vim -c :PlugInstall #Calls vim-plug to install plugins
 
-printf "$(tput setaf 1)You must change your shell to ZSH (Z-Shell) now$(tput sgr0)\n"
+printf "${RED}You must change your shell to ZSH (Z-Shell) now${NORMAL}\n"
 while true; do 
-    read -p "$(tput setaf 1)Would you like me to change this for you? (Y/n)$(tput sgr0)" yn
+    read -p "${CYAN}Would you like me to change this for you? (Y/n)${NORMAL}" yn
     case $yn in
         [Yy]* ) chsh -s $(which zsh);
-            printf "$(tput setaf 1)Your shell is now ZSH!\n"; break;;
-        [Nn]* ) printf "$(tput setaf 1)Ok, I am not setting zsh as your shell$(tput sgr0)\n"; break;;
-        * ) printf "$(tput setaf 1)Please enter Y or N$(tput sgr0)\n";;
+            printf "${CYAN}Your shell is now ZSH!\n"; break;;
+        [Nn]* ) printf "${BLUE}Ok, I am not setting zsh as your shell${NORMAL}\n"; break;;
+        "" ) chsh -s $(which zsh);
+            printf "${CYAN}Your shell is now ZSH!\n"; break;;
+        * ) printf "${BLUE}Please answer \"y\"es or \"n\"o${NORMAL}\n";;
     esac
 done
 
-printf "$(tput setaf 1)Please restart your shell, once you reopen, please enter option \"0\"$(tput sgr0)\n"
-printf "$(tput setaf 1)If you do not, it will overwrite your your .zshrc file with a default one$(tput sgr0)\n"
+printf "${RED}Please restart your shell, once you reopen, please enter option \"0\"${NORMAL}\n"
+printf "${RED}If you do not, it will overwrite your your .zshrc file with a default one${NORMAL}\n"
 exit
