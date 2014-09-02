f5bdad3d5de3276c569742328047e436ca7c883b
hackinginformation <chase.hallsted2012@gmail.com>
2014-09-02 12:12:19 -0500

    I believe everything is fixed in the code

--- install.sh
+++ install.sh
@@ -34,12 +34,12 @@ printf "$(tput setaf 1)${BLUE}You need to install git, zsh, wget, curl and vim, 
 printf "$(tput setaf 1)This will only work on ubuntu based OS's currently$(tput sgr0)\n"
 printf "$(tput setaf 1)Select \"n\" if you are not useing an ubuntu based OS$(tput sgr0)\n"
 while true; do 
-    read -p "$(tput setaf 1)Would you like me to install them for you? (Y/n)$(tput sgr0)" -i "Y" yn
+    read -p "$(tput setaf 1)Would you like me to install them for you? (Y/n)$(tput sgr0)" yn
     case $yn in
-        [Yy]* ) sudo apt-get -y install git zsh wget curl vim> /dev/null;
-            printf "$(tput setaf 1)Ok I am installing git, zsh and vim for you$(tput sgr0)\n"; break;; 
+        [Yy]* ) printf "$(tput setaf 1)Ok I am installing git, zsh and vim for you$(tput sgr0)\n";
+            sudo apt-get -y install git zsh wget curl vim > /dev/null; break;;
         [Nn]* ) printf "Ok, skipping the installs$(tput sgr0)\n"; break;;
-        * ) printf "$(tput setaf 1)Please answer yes or no, enter will auto accept yes$(tput sgr0)\n";; 
+        * ) printf "$(tput setaf 1)Please answer yes or no$(tput sgr0)\n";; 
     esac
 done
 
@@ -66,21 +66,20 @@ cp $GIT_DIRECTORY/mydotfiles/zsh/.zshrc .
 sed -i "s,RePlAcE,$GIT_DIRECTORY,g" "$HOME/.zshrc"
 ln -sf $GIT_DIRECTORY/mydotfiles/vim/.vimrc ~/.vimrc #Symlinks for the .vimrc file
 mkdir -p ~/.vim/autoload # Preps for vim-plug to be installed
-curl --silent -fLos ~/.vim/autoload/plug.vim \ 
-    https://raw.githubusercontent.com/junegun/vim-plug/master/plug.vim #Installs vim-plug to install remaining plugins
-
+curl --silent -fLo ~/.vim/autoload/plug.vim \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 printf "$(tput setaf 1)You are about to be presented with a vim script updater$(tput sgr0)\n"
 printf "$(tput setaf 1)Please wait for it to complete, then run :q two to exit and continue$(tput sgr0)\n"
 vim -c :PlugInstall #Calls vim-plug to install plugins ###BROKEN
 
 printf "$(tput setaf 1)You must change your shell to ZSH (Z-Shell) now$(tput sgr0)\n"
 while true; do 
-    read -p "$(tput setaf 1)Would you like me to change this for you? (Y/n)$(tput sgr0)" -i "Y" yn
+    read -p "$(tput setaf 1)Would you like me to change this for you? (Y/n)$(tput sgr0)" yn
     case $yn in
         [Yy]* ) chsh -s $(which zsh);
             printf "$(tput setaf 1)Your shell is now ZSH!\n"; break;;
         [Nn]* ) printf "$(tput setaf 1)Ok, I am not setting zsh as your shell$(tput sgr0)\n"; break;;
-        * ) printf "$(tput setaf 1)Please enter Y or N, enter will auto accept Yes$(tput sgr0)\n";;
+        * ) printf "$(tput setaf 1)Please enter Y or N$(tput sgr0)\n";;
     esac
 done
 
