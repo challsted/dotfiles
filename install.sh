7a2764fcffa9008ce6475725ab0a7ba7346c5048
Chase Hallsted <chase.hallsted2012@gmail.com>
2014-09-01 21:51:41 -0500

    fixed linebreak bug, still have issue with mkdir var

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#!/usr/bin/bash
 #
 # An attempt at makeing an install for the files I upload
 # YOU WILL NEED TO CHMOD THIS FILE (chmod +x install.sh)
@@ -9,28 +10,28 @@ printf "You need to install git, zsh, and vim, if you havnt already\n"
 printf "This will only work on ubuntu based OS's currently\n"
 printf "Select \"n\" if you are not useing an ubuntu based OS\n"
 while true; do # This simply prompts the user if they want the script to install git, zsh and vim
-    read -e -p "Would you like me to install them for you? (Y/n)" -i "Y" yn
+    read -e -p "Would you like me to install them for you? (Y/n)\n" -i "Y" yn
     case $yn in
         [Yy]* ) sudo apt-get -y install git zsh vim;
-            printf "Ok I am installing git, zsh and vim for you"; break;; 
-        [Nn]* ) printf "Ok, skipping the installs"; break;;
-        * ) printf "Please answer yes or no, enter will auto accept yes";; 
+            printf "Ok I am installing git, zsh and vim for you\n"; break;; 
+        [Nn]* ) printf "Ok, skipping the installs\n"; break;;
+        * ) printf "Please answer yes or no, enter will auto accept yes\n";; 
     esac
 done
 
 if [[ ! -d ~/git/ ]] || [[ ! -d ~/.git/ ]]; then
     while true; do
-        read -e -p "Do you want a reguler folder for git or a hidden folder? (R/h)" -i "R" rh
+        read -e -p "Do you want a reguler folder for git or a hidden folder? (R/h)\n" -i "R" rh
         case $rh in
             [Rr]* ) if [[ ! -d ~/git/ ]]; then 
                 GIT_DIRECTORY=mkdir ~/git/ && cd ~/$GIT_DIRECTORY/
             fi; 
-                printf "Ok, I have created $GIT_DIRECTORY for you"; break;;
+                printf "Ok, I have created $GIT_DIRECTORY for you\n"; break;;
             [Hh]* ) if [[ ! -d ~/.git/ ]]; then
                 GIT_DIRECTORY=mkdir ~/.git/ && cd ~/$GIT_DIRECTORY/
             fi;
                 printf "Ok, I have created $GIT_DIRECTORY for you"; break;;
-            * ) printf "You must answer with \"R\", \"H\", or the full word(s) enter will auto accept regular";;
+            * ) printf "You must answer with \"R\", \"H\", or the full word(s) enter will auto accept regular\n";;
         esac
     done
 fi
@@ -45,11 +46,11 @@ git clone https://github.com/hackinginformation/mydotfiles.git
 
 printf "You must change your shell to ZSH (Z-Shell) now\n"
 while true; do 
-    read -e -p "Would you like me to change this for you? (Y/n)" -i "Y" yn
+    read -e -p "Would you like me to change this for you? (Y/n)\n" -i "Y" yn
     case $yn in
         [Yy]* ) printf "Your shell is now ZSH!"; chsh -s $(which zsh); break;;
-        [Nn]* ) printf "Ok, I am not setting zsh as your shell"; break;;
-        * ) printf "Please enter Y or N, enter will auto accept Yes";;
+        [Nn]* ) printf "Ok, I am not setting zsh as your shell\n"; break;;
+        * ) printf "Please enter Y or N, enter will auto accept Yes\n";;
     esac
 done
 
