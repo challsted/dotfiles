71f1bedb75d5ac6a815eb8f39cbc97beca802fd8
Chase <chase.hallsted@gmail.com>
2015-10-02 11:24:47 -0500

    Playing with Git Aliases
    Experimental changes for git aliases

--- zsh/.alias.zsh
+++ zsh/.alias.zsh
@@ -11,30 +11,32 @@
 #Directory Stuff
 alias ll="ls -larth"  #All files, "long" with reverse time sorting and human readable sizes; added colros for terminals that dont auto do this
 alias lh="ls -d .*" # ONLY show hidden files/folders this is genius (stolen); added colors for terminals that dont auto do this
-alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'" #Stolen from the Internet, makes a tree out of current dirs 
+alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'" #Stolen from the Internet, makes a tree out of current dirs
 
-#OS Specific 
+#OS Specific
 alias ubuntuupdate="sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove"  #Fully Update Ubuntu, assumes yes, except for auto-remove
 alias ubinstall="sudo apt-get install $2" #install stuff with 1 word prefix
 alias ubremove="sudo apt-get remove $2" #Remove stuff with 1 word prefix
 
 #ZSH Commands to make editing stuff easier
 alias configzsh="vim ~/.zshrc"  #One command to edit main config file from anywhere
-alias sourcezsh="source ~/.zshrc"  #One command to source the main config file from anywhere (. .zshrc equivilent from bash) 
+alias sourcezsh="source ~/.zshrc"  #One command to source the main config file from anywhere (. .zshrc equivilent from bash)
 
 #SSH Stuff
 alias sshx="ssh -c arcfour,blowfish -XC" #Add a connection and a thing you want forwarded to user this
 
 #GIT Stuff
 alias gs="git status"
-alias ga="git add ." #Add current dir to git queue
+alias ga="git add" #ga <file name>. Add file name after ga
 alias gA="git add -A" #Add everything, adds, deletes, changes to the git queue
-alias gc="git commit -m" #Commits current queue to git. Requires you to type a commit message
+alias gc="git commit" #Commits current queue to git. Requires you to type a commit message
+alias gm="git commit -m" #Commits current queue to git. Requires you to type a commit message
 alias gp="git pull" #Pulls the updates from remote to local
 alias gP="git push origin" #Pushs to remote branch (given after gP) ex. gP master will git push origin master
 alias co="git checkout" #Checks out a branch given after co
 alias cod="git checkout development" #Checks out the development branch
-alias cop="git checkout puppetdev" #Checks out the puppetdev branch
+alias com="git checkout master" #Checks out the master branch
+alias cop="git checkout production" #Checks out the production branch
 export GIT_EDITOR=vim #Export git commit message editor (if you dont use -m)
 
 #Application Stuff
@@ -54,5 +56,5 @@ alias youtube-update="sudo pip install mps-youtube --upgrade"
 #Stupid Stuff
 alias doit="sudo !!"
 
-#Pull everything down!!!
-alias pullall="for REPO in "`find ~/git -maxdepth 0 -type d`"; do (cd "$REPO"; git pull); done;"
+#Pull everything down!!! (Experimental)
+alias pullall="for REPO in `find ~/git -maxdepth 1 -type d`; do (cd "$REPO" && git pull origin master); done;"
