122d365e758aa52dc40f5447a3477cfd15469015
Chase Hallsted <chase.hallsted@gmail.com>
2015-11-26 21:54:24 -0600

    Just high level bullets

---
+++ scripts/custom_mutt.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Set Version of Mutt (hard code for now work on variable later)
+# Download Vanilla Mutt
+# expand archive
+# download patches
+# Add Later: handle errors appropriately
+# Compile Mutt
+# Add Later: handle errors appropriately
+# Install Mutt
+# Add Later: Optional Compile Flags
