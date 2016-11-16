0d4c05fd7aeaa19fb06e222e3ab872eb66754dd6
Chase Hallsted <chase.hallsted@gmail.com>
2016-11-16 12:37:50 -0600

    Added Conditional's to a lot of stuff, prepping for release

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -13,6 +13,9 @@ alias lh="ls -d .*" # ONLY show hidden files/folders this is genius (stolen); ad
 alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'" #Stolen from the Internet, makes a tree out of current dirs
 
 #OS Specific
+# TODO [Enhancement] Has OS - Write Function to test what OS user is on
+# TODO [Dependency] e<thing> (OS Specific) - This relies on hasOS function being written
+# TODO [Testing] e<thing> (OS Specific) Need to test this on all OS's I run
 #eupdate - easy update
 #einstall - easy install
 #eremove - easy remove
@@ -37,7 +40,6 @@ else
     alias einstall="echo no idea what your running!"
     alias eremove="echo no idea what your running!"
 fi
-
 
 #SSH Stuff
 # TODO [Testing] sshx - Validate this even works
@@ -68,83 +70,26 @@ fi
 # Set multiple values ONLY if NVim is installed
 # Set MANPAGER to open man pages in NVim or Vim
 # Set system wide EDITOR and GIT_EDITOR as appropriate
-# Alias "vi" to nvim or vim as needed
+# Alias "vi" to NeoVim, leaving "vim" to open vim
 if type nvim > /dev/null; then
     export MANPAGER="env MAN_PN=1 nvim -M +MANPAGER -"
     export EDITOR="nvim"
-    export GIT_EDITOR=nvim 
+    export GIT_EDITOR="nvim"
     alias vi="nvim"
 else
     export MANPAGER="env MAN_PN=1 vim -M +MANPAGER -"
     export EDITOR="vim"
-    export GIT_EDITOR=vim 
-    alias vi="vim"
+    export GIT_EDITOR="vim"
 fi
-
-
-# A small function to check if a given  value is a number 
-#   Bash dosnt have a clean way to do this that I can find
-# Returns "0" if the value is NOT a number
-# Returns "1" if the value is a number
-isNum(){
-    awk -v NUM="$1" 'BEGIN {print (NUM == NUM + 0)}';
-}
 
 #TMux Stuff
 if type tmux > /dev/null; then
     alias ntmux="tmux new-session -s $2" #Allows you to type "ntmux <random>" and it will open a new tmux session
     alias atmux="tmux attach-session -t $2" #Allows you to attach to a tmux session at willA
     alias ltmux="tmux list-sessions" #An easy way to list available tmux sessions
-
-    # Attempt at making a "Quick TMux Session Connector"
-    # "qtmux" - Open a new session with the value given to "QTMUX_SESSION_NAME"
-    # "qtmux <session name" - Attach to a pre-created session
-    # "qtmux <session id> - Attach to a pre-created session based on id (starting at 1)
-    #   TODO [Feature] qtmux() needs to fail cleanly if session name or id given are not available
-    #   TODO [Feature] qtmux() needs a way to be debugged (not clear variables after being called)
-    #   TODO [Enhancement] qtmux() convert to getopts?
-    qtmux(){
-        local QTMUX_SESSION_NAME="random"
-        local QTMUX_USER_INPUT="$2"
-        local IS_NUM=$(isNum $2) #Checks if user passed in a number or a word
-        local QTMUX_DEBUG="" # WRITE THIS TO FULFIL DEBUG NEEDS!(?)
-
-        if [[ $IS_NUM == "1" ]]; then
-            local QTMUX_NUMBER=1
-        fi
-
-        if [[ -z $QTMUX_USER_INPUT ]]; then # If user passes input
-            if  [[ $QTMUX_NUMBER == "1" ]]; then # First check if the value given is a number (session id)
-                local QTMUX_SESSION_AVAILABLE=$(tmux list-sessions | awk -v QTMUX_AWK=$QTMUX_USER_INPUT 'NR==QTMUX_AWK' | awk '{print $1}' | wc -l)
-                local QTMUS_SESSION_FNAME=$(tmux list-sessions | awk -v QTMUX_AWK=$QTMUX_USER_INPUT 'NR==QTMUX_AWK' | awk '{print $1}')
-                if [[ $QTMUX_SESSION_AVAILABLE  == "1" ]]; then
-                    tmux attach-session -t $QTMUX_SESSION_FNAME
-                else
-                    echo -e "I'm sorry, the Session ID you passed: \n\t$QTMUX_USER_INPUT \nAppears to not exist"
-                fi
-            else
-                local QTMUX_SESSION_AVAILABLE=$(tmux list-sessions | grep $QTMUX_USER_INPUT | awk {'print $1'} | wc -l)
-                local QTMUX_SESSION_AVAILABLE=$(tmux list-sessions | grep $QTMUX_USER_INPUT | awk {'print $1'})
-                if [[ $QTMUX_SESSION_AVAILABLE  == "1" ]]; then
-                    tmux attach-session -t $QTMUX_SESSION_FNAME
-                else
-                    echo -e "I'm sorry, the Session Name you passed: \n\t$QTMUX_USER_INPUT \nAppears to not exist"
-                fi
-            fi
-        else
-            tmux new-session -s $QTMUX_SESSION_NAME
-        fi
-       
-        # Clean up variables only if --debug isnt passed
-        if [[ ! -z $QTMUX_DEBUG ]]; then
-            QTMUX_SESSION_AVAILABLE=""
-            QTMUX_SESSION_ID=""
-            QTMUX_SESSION_NAME=""
-            QTMUX_USER_INPUT=""
-        fi
-    }
 fi
 
+#Random Applications
 #If you want this, install with: pip install mps-youtube
 if type mpsyt > /dev/null; then
     alias youtube="mpsyt"
@@ -153,4 +98,3 @@ fi
 
 #Stupid Stuff
 alias doit="sudo !!"
-
