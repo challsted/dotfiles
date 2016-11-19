b8bbdde09b48b01035e673b08fbf1ee44523f987
Chase Hallsted <chase.hallsted@gmail.com>
2016-11-19 15:00:23 -0600

    Removed Globals from base rc, added comments

--- zsh/globals.zsh
+++ zsh/globals.zsh
@@ -4,6 +4,9 @@
 #
 #Purpose: This file is to be source from the ~/.zshrc file and its purpose is to keep as many of the "Global" changes as possible in 1 spot.
 
+#ONLY UPDATE PATH FROM HERE
+PATH="/usr/local/heroku/bin:~/.config/bspwm/:$PATH"
+
 #Attempts to set xterm to xterm-256 colors
 #This works for xfce4-terminal
 case "$TERM" in
@@ -36,3 +39,29 @@ else
     export VISUAL="vim"
     alias vi="vim"
 fi
+
+#This sets the keyboard layout to QWERTY, but after pressing the right control and the right shift (at the same time) key it toggles the keyboard layout to be Dvoark
+#It also turns the CAPS LOCK Key into a Control Key
+#It Also turns on/off the Scroll Lock LED Off = QWERTY On=Dvorak
+#Secondly, it turns caps lock (if held) into a ctrl key
+if type setxkbmap > /dev/null; then
+    setxkbmap -layout "us,us" -variant ",dvorak" -option "grp:rctrl_rshift_toggle,grp_led:scroll,caps:ctrl_modifier"
+    setxkbmap -option 'caps:ctrl_modifier'
+fi
+
+# This turns caps lock (if only pressed) into Esc
+# github.com/alols/xcape
+if type xcape > /dev/null; then
+    xcape -e 'Caps_Lock=Escape'
+fi
+
+# Enable RBEnv on boot
+# github.com/rbenv/rbenv
+if type rbenv > /dev/null; then
+    eval "$(rbenv init -)"
+fi
+
+# For somereason Im having issues where my XDG's are not set...
+ if [ -z $XDG_CONFIG_HOME ]; then
+     export XDG_CONFIG_HOME="$HOME/.config"
+ fi
